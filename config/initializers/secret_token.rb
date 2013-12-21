3d958f8cda146d49ffebad263cddf616bee29a0c
John Brunton <john_brunton@hotmail.co.uk>
2013-12-21 22:55:49 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IosdemoApi::Application.config.secret_key_base = '20f1ad5b95b25bc245b7560461f3f0521c8c30d23abd33172f00685b3c4750ab85738ee2fce56338ad9bfabe77ff668d1d4add0327175b52f0cda6b1f02496a9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IosdemoApi::Application.config.session_store :cookie_store, key: '_iosdemo-api_session'
